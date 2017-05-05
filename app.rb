3187b496c2d535d3b33a51ffec3daa0754fdf902
Elan Baharie <0elan1@gmail.com>
2017-05-05 13:03:14 -0700

    basic front end in place to add vols and projects

--- app.rb
+++ app.rb
@@ -12,7 +12,48 @@ get('/') do
   erb(:index)
 end
 
+get('/volunteers') do
+  @volunteers = Volunteers.all()
+  erb(:volunteers)
+end
 
+get('/volunteers/new') do
+  @volunteers = Volunteers.all()
+  erb(:volunteers)
+end
+
+post('/volunteers') do
+  name = params.fetch('name')
+  address = params.fetch('address')
+  details = params.fetch('details')
+  project_id = params.fetch('project_id').to_i()
+  new_volunteer = Volunteers.new({:id => nil, :name => name, :address => address, :details => details, :project_id => project_id})
+  new_volunteer.save()
+  erb(:success)
+end
+
+get('/success') do
+  erb(:success)
+end
+
+get('/projects') do
+  @projects = Projects.all()
+  erb(:projects)
+end
+
+get('/projects/new') do
+  @projects = Projects.all()
+  erb(:projects)
+end
+
+post('/projects') do
+  name = params.fetch('name')
+  location = params.fetch('location')
+  details = params.fetch('details')
+  new_project = Projects.new({:id => nil, :name => name, :location => location, :details => details})
+  new_project.save()
+  erb(:success)
+end
 
 # post('/first_form') do
 #   @first_name = params.fetch('first-name')
